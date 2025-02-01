5aae84e2ce94a404fccb387ad51aa0a405b7742d
leapy <143796566+leafisnothere@users.noreply.github.com>
2025-02-01 05:51:16 +0000

    Updated Index.js

--- index.js
+++ index.js
@@ -15,13 +15,11 @@ new CommandKit({
   commandsPath: `${__dirname}/Commands`,
 })
 
-
-
 client.login(process.env.DiscordBOT);
 
 client.on('ready', (C) => {
   console.log(`Bot is ready, logged in as ${C.user.username}`)
   client.user.setStatus(PresenceUpdateStatus.Online);
-  client.user.setPresence(Status)
-  client.user.setActivity(`as avalible in ${client.guilds.cache.size} Servers!`, { type: ActivityType.Watching });
+client.user.setPresence(Status)
+client.user.setActivity(`as avalible in ${client.guilds.cache.size} Servers!`, { type: ActivityType.Watching });
 })
